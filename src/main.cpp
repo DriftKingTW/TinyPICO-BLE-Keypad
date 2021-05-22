ce1769228d39f73d7d43cf31bb5ceb84f005acfa
DriftKingTW <driftkingtw@gmail.com>
2021-05-23 00:51:34 +0800

    Add basic matrix keyboard feature

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,8 @@
 #define AUTHOR "DriftKingTW"
 #define ACTIVE LOW
 
-// #define ROWS 5
-// #define COLS 7
+#define ROWS 5
+#define COLS 7
 
 U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/U8X8_PIN_NONE);
 BleKeyboard bleKeyboard(BLE_NAME, AUTHOR);
@@ -16,66 +16,93 @@ TinyPICO tp = TinyPICO();
 
 // Stucture for key stroke
 struct Key {
-    byte pin;
     uint8_t keyStroke;
     bool state;
     char *keyInfo;
 };
 
-// struct Key key1;
-// struct Key key2;
-// struct Key key3;
-// struct Key key4;
-// struct Key key5;
-// struct Key key6;
-// struct Key key7;
-// struct Key key8;
-// struct Key key9;
-// struct Key key10;
-// struct Key key11;
-// struct Key key12;
-// struct Key key13;
-// struct Key key14;
-// struct Key key15;
-// struct Key key16;
-// struct Key key17;
-// struct Key key18;
-// struct Key key19;
-// struct Key key20;
-// struct Key key21;
-// struct Key key22;
-// struct Key key23;
-// struct Key key24;
-// struct Key key25;
-// struct Key key26;
-// struct Key key27;
-// struct Key key28;
-// struct Key key29;
-// struct Key key30;
-// struct Key key31;
+struct Key key1;
+struct Key key2;
+struct Key key3;
+struct Key key4;
+struct Key key5;
+struct Key key6;
+struct Key key7;
+struct Key key8;
+struct Key key9;
+struct Key key10;
+struct Key key11;
+struct Key key12;
+struct Key key13;
+struct Key key14;
+struct Key key15;
+struct Key key16;
+struct Key key17;
+struct Key key18;
+struct Key key19;
+struct Key key20;
+struct Key key21;
+struct Key key22;
+struct Key key23;
+struct Key key24;
+struct Key key25;
+struct Key key26;
+struct Key key27;
+struct Key key28;
+struct Key key29;
+struct Key key30;
+struct Key key31;
+struct Key dummy;
 
-// Key keyMap[ROWS][COLS] = {{key1, key2, key3, key4, key5, key6},
-//                           {key7, key8, key9, key10, key11, key12, key13},
-//                           {key14, key15, key16, key17, key18, key19},
-//                           {key20, key21, key22, key23, key24, key25, key26},
-//                           {key27, key28, key29, key30, key31}};
+Key keyMap[ROWS][COLS] = {{key1, key2, key3, key4, key5, key6, dummy},
+                          {key7, key8, key9, key10, key11, key12, key13},
+                          {key14, key15, key16, key17, key18, key19, dummy},
+                          {key20, key21, key22, key23, key24, key25, key26},
+                          {key27, key28, key29, dummy, key30, dummy, key31}};
 
-// uint8_t keyLayout[ROWS][COLS] = {{KEY_ESC, 49, 50, 51, 52, 53},
-//                                  {KEY_TAB, 'q', 'w', 'e', 'r', 't', 127},
-//                                  {KEY_LEFT_CTRL, 'a', 's', 'd', 'f', 'g'},
-//                                  {KEY_LEFT_SHIFT, 'z', 'x', 'c', 'v', 'b', 13},
-//                                  {'h', KEY_LEFT_ALT, KEY_LEFT_GUI, 32, 'h'}};
+uint8_t keyLayout[ROWS][COLS] = {
+    {KEY_ESC, 49, 50, 51, 52, 53, 49},
+    {KEY_TAB, 'q', 'w', 'e', 'r', 't', 8},
+    {KEY_LEFT_CTRL, 'a', 's', 'd', 'f', 'g', 49},
+    {KEY_LEFT_SHIFT, 'z', 'x', 'c', 'v', 'b', KEY_RETURN},
+    {'h', KEY_LEFT_ALT, KEY_LEFT_GUI, 49, 32, 'h', 49}};
 
-// byte inputs[] = {23, 19, 18, 5, 32, 33, 25};  // declaring inputs and outputs
-// const int inputCount = sizeof(inputs) / sizeof(inputs[0]);
-// byte outputs[] = {4, 17, 15, 27, 26};  // row
-// const int outputCount = sizeof(outputs) / sizeof(outputs[0]);
+char *keyInfo[ROWS][COLS] = {
+    {(char *)"ESC", (char *)"1", (char *)"2", (char *)"3", (char *)"4",
+     (char *)"5"},
+
+    {(char *)"TAB", (char *)"Q", (char *)"W", (char *)"E", (char *)"R",
+     (char *)"T", (char *)"DELETE"},
+
+    {(char *)"CTRL", (char *)"A", (char *)"S", (char *)"D", (char *)"F",
+     (char *)"G"},
+
+    {(char *)"SHIFT", (char *)"Z", (char *)"X", (char *)"C", (char *)"V",
+     (char *)"B", (char *)"RETURN"},
+
+    {(char *)"H", (char *)"OPTION", (char *)"COMMAND", (char *)"SPACE",
+     (char *)"H"}};
+
+byte inputs[] = {23, 19, 18, 5, 32, 33, 25};  // declaring inputs and outputs
+const int inputCount = sizeof(inputs) / sizeof(inputs[0]);
+byte outputs[] = {4, 14, 15, 27, 26};  // row
+const int outputCount = sizeof(outputs) / sizeof(outputs[0]);
 
 // Function declaration
 void initKeys();
 void lightSleepSetup();
 void renderScreen(char *msg);
-void handleKeyPress(Key &key);
+void keyPress(Key &key);
+void keyRelease(Key &key);
+void showBatteryState();
+
+float getBatteryPercentage() {
+    float minVoltage = 3.700;
+    float fullVolatge = 4.200;
+    float batteryVolatage = tp.GetBatteryVoltage();
+
+    return (batteryVolatage - minVoltage) / (fullVolatge - minVoltage) * 100;
+}
 
 void setup() {
     Serial.begin(115200);
@@ -96,9 +123,9 @@ void setup() {
     lightSleepSetup();
 
     // Sleep test
-    Serial.println("Go to sleep after 1 sec!");
-    delay(1000);
-    esp_light_sleep_start();
+    // Serial.println("Go to sleep after 1 sec!");
+    // delay(1000);
+    // esp_light_sleep_start();
 }
 
 void loop() {
@@ -108,9 +135,28 @@ void loop() {
 
     // Check every keystroke is pressed or not
     while (bleKeyboard.isConnected()) {
-        handleKeyPress(KeyCmd);
-        handleKeyPress(KeyC);
-        handleKeyPress(KeyV);
+        for (int r = 0; r < ROWS; r++) {
+            digitalWrite(outputs[r], LOW);  // Setting one row low
+            delayMicroseconds(5);  // Time for electronics to settle down
+
+            for (int c = 0; c < COLS; c++) {
+                if (digitalRead(inputs[c]) == ACTIVE) {
+                    // Calling keyPressed function if one of the inputs
+                    // reads low
+                    if (r == 4 && c == 0) {
+                        // Show battery on LED if the specific key is pressed
+                        showBatteryState();
+                    } else {
+                        keyPress(keyMap[r][c]);
+                    }
+                } else {
+                    keyRelease(keyMap[r][c]);
+                }
+            }
+
+            digitalWrite(outputs[r], HIGH);  // Setting the row back to high
+            delayMicroseconds(5);
+        }
         delay(10);
     }
 
@@ -122,41 +168,25 @@ void loop() {
  *
  */
 void initKeys() {
-    KeyC.pin = 32;
-    KeyC.keyStroke = 'b';
-    KeyC.state = false;
-    KeyC.keyInfo = (char *)"B";
+    // KeyC.keyStroke = 'b';
+    // KeyC.state = false;
 
-    KeyV.pin = 33;
-    KeyV.keyStroke = 'e';
-    KeyV.state = false;
-    KeyV.keyInfo = (char *)"E";
+    for (int i = 0; i < outputCount; i++) {
+        pinMode(outputs[i], OUTPUT);
+        digitalWrite(outputs[i], HIGH);
+    }
 
-    KeyCmd.pin = 14;
-    // KeyCmd.keyStroke = KEY_LEFT_GUI;
-    KeyCmd.keyStroke = 32;
-    KeyCmd.state = false;
-    KeyCmd.keyInfo = (char *)"Space";
+    for (int i = 0; i < inputCount; i++) {
+        pinMode(inputs[i], INPUT_PULLUP);
+    }
 
-    pinMode(KeyCmd.pin, INPUT_PULLUP);
-    pinMode(KeyC.pin, INPUT_PULLUP);
-    pinMode(KeyV.pin, INPUT_PULLUP);
-
-    // for (int i = 0; i < outputCount; i++) {
-    //     pinMode(outputs[i], OUTPUT);
-    //     digitalWrite(outputs[i], HIGH);
-    // }
-
-    // for (int i = 0; i < inputCount; i++) {
-    //     pinMode(inputs[i], INPUT_PULLUP);
-    // }
-
-    // for (short r = 0; r < ROWS; r++) {
-    //     for (short c = 0; c < COLS; c++) {
-    //         keyMap[r][c].keyStroke = keyLayout[r][c];
-    //         keyMap[r][c].state = false;
-    //     }
-    // }
+    for (int r = 0; r < ROWS; r++) {
+        for (int c = 0; c < COLS; c++) {
+            keyMap[r][c].keyStroke = keyLayout[r][c];
+            keyMap[r][c].keyInfo = keyInfo[r][c];
+            keyMap[r][c].state = false;
+        }
+    }
 }
 
 /**
@@ -171,24 +201,54 @@ void lightSleepSetup() {
 }
 
 /**
- * Handle the button press event and returns the updated button state.
+ * Press key
  *
  * @param {Key} key the key to be pressed
  */
-void handleKeyPress(Key &key) {
-    if (digitalRead(key.pin) == ACTIVE) {
-        if (key.state == false) {
-            bleKeyboard.press(key.keyStroke);
-        }
-        key.state = true;
-        renderScreen(key.keyInfo);
-        return;
-    } else if (key.state == true) {
+void keyPress(Key &key) {
+    if (key.state == false) {
+        bleKeyboard.press(key.keyStroke);
+    }
+    key.state = true;
+    // renderScreen(key.keyInfo);
+}
+
+/**
+ * Release key
+ *
+ * @param {Key} key the key to be released
+ */
+void keyRelease(Key &key) {
+    if (key.state == true) {
         bleKeyboard.release(key.keyStroke);
-        renderScreen((char *)"Waiting for input...");
+        // renderScreen((char *)"Waiting for input...");
     }
     key.state = false;
     return;
+}
+
+/**
+ * Show battery state using onboard RGB LED
+ *
+ */
+void showBatteryState() {
+    tp.DotStar_SetPower(true);
+    float batteryV = getBatteryPercentage();
+    Serial.println((String)batteryV + "%");
+    tp.DotStar_SetBrightness(50);
+    if (batteryV > 90) {
+        tp.DotStar_SetPixelColor(0, 255, 0);
+    } else if (batteryV > 75) {
+        tp.DotStar_SetPixelColor(0, 200, 0);
+    } else if (batteryV > 50) {
+        tp.DotStar_SetPixelColor(219, 245, 0);
+    } else if (batteryV > 25) {
+        tp.DotStar_SetPixelColor(236, 141, 0);
+    } else {
+        tp.DotStar_SetPixelColor(255, 0, 0);
+    }
+    delay(1000);
+    tp.DotStar_SetPower(false);
 }
 
 /**
