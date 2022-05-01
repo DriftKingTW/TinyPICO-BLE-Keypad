1011a718d558ff723e812735b1eb2dafb9fea3da
DriftKingTW <driftkingtw@gmail.com>
2022-05-01 16:31:02 +0800

    feat: optimize code and add battery related-feature

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,17 @@
 #include <TinyPICO.h>
 #include <U8g2lib.h>
 
-#define BLE_NAME "TinyPICO"
+#include <cstring>
+
+#define BLE_NAME "TinyPICO BLE"
 #define AUTHOR "DriftKingTW"
 #define ACTIVE LOW
 
 #define ROWS 5
 #define COLS 7
 
-U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/U8X8_PIN_NONE);
+// U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/U8X8_PIN_NONE);
+U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C u8g2(U8G2_R0, /* reset=*/U8X8_PIN_NONE);
 BleKeyboard bleKeyboard(BLE_NAME, AUTHOR);
 TinyPICO tp = TinyPICO();
 
@@ -18,10 +21,10 @@ TinyPICO tp = TinyPICO();
 struct Key {
     uint8_t keyStroke;
     bool state;
-    char *keyInfo;
-};
-
-Key key1, key2, key3, key4, key5, key6, key7, key8, key9, key10, key11, key12, key13, key14, key15, key16, key17, key18, key19, key20, key21, key22, key23, key24, key25, key26, key27, key28, key29, key30, key31, dummy;
+    String keyInfo;
+} key1, key2, key3, key4, key5, key6, key7, key8, key9, key10, key11, key12,
+    key13, key14, key15, key16, key17, key18, key19, key20, key21, key22, key23,
+    key24, key25, key26, key27, key28, key29, key30, key31, dummy;
 
 Key keyMap[ROWS][COLS] = {{key1, key2, key3, key4, key5, key6, dummy},
                           {key7, key8, key9, key10, key11, key12, key13},
@@ -29,28 +32,43 @@ Key keyMap[ROWS][COLS] = {{key1, key2, key3, key4, key5, key6, dummy},
                           {key20, key21, key22, key23, key24, key25, key26},
                           {key27, key28, key29, dummy, key30, dummy, key31}};
 
-uint8_t keyLayout[ROWS][COLS] = {
+uint8_t keyLayout_1[ROWS][COLS] = {
     {KEY_ESC, 49, 50, 51, 52, 53, 49},
     {KEY_TAB, 'q', 'w', 'e', 'r', 't', 8},
     {KEY_LEFT_CTRL, 'a', 's', 'd', 'f', 'g', 49},
     {KEY_LEFT_SHIFT, 'z', 'x', 'c', 'v', 'b', KEY_RETURN},
     {'h', KEY_LEFT_ALT, KEY_LEFT_GUI, 49, 32, 'h', 49}};
 
-char *keyInfo[ROWS][COLS] = {
-    {(char *)"ESC", (char *)"1", (char *)"2", (char *)"3", (char *)"4",
-     (char *)"5"},
+uint8_t keyLayout_2[ROWS][COLS] = {
+    {KEY_ESC, 49, 50, 51, 52, 53, 49},
+    {KEY_TAB, 's', 'w', 'e', '[', ']', 8},
+    {KEY_LEFT_CTRL, '[', ']', 'l', 'b', 'g', 49},
+    {KEY_LEFT_SHIFT, 'z', 'x', 'c', 'v', 'b', KEY_RETURN},
+    {'h', KEY_LEFT_ALT, KEY_LEFT_GUI, 49, 32, KEY_LEFT_GUI, 49}};
 
-    {(char *)"TAB", (char *)"Q", (char *)"W", (char *)"E", (char *)"R",
-     (char *)"T", (char *)"DELETE"},
+byte currentLayout = 2;
 
-    {(char *)"CTRL", (char *)"A", (char *)"S", (char *)"D", (char *)"F",
-     (char *)"G"},
+String keyInfo_1[ROWS][COLS] = {
+    {"ESC", "1", "2", "3", "4", "5", "NULL"},
 
-    {(char *)"SHIFT", (char *)"Z", (char *)"X", (char *)"C", (char *)"V",
-     (char *)"B", (char *)"RETURN"},
+    {"TAB", "Q", "W", "E", "R", "T", "DELETE"},
 
-    {(char *)"H", (char *)"OPTION", (char *)"COMMAND", (char *)"SPACE",
-     (char *)"H"}};
+    {"CTRL", "A", "S", "D", "F", "G", "NULL"},
+
+    {"SHIFT", "Z", "X", "C", "V", "B", "RETURN"},
+
+    {"FN", "OPTION", "COMMAND", "NULL", "SPACE", "H", "NULL"}};
+
+String keyInfo_2[ROWS][COLS] = {
+    {"ESC", "1", "2", "3", "4", "5", "NULL"},
+
+    {"TAB", "Select", "W", "Eraser", "Brush Down", "Brush Up", "DELETE"},
+
+    {"CTRL", "Brush Down", "Brush Up", "Layers", "Brush", "G", "NULL"},
+
+    {"SHIFT", "Z", "X", "C(Colors)", "V(Transform)", "B", "RETURN"},
+
+    {"FN", "OPTION", "COMMAND", "NULL", "SPACE", "H", "NULL"}};
 
 byte inputs[] = {23, 19, 18, 5, 32, 33, 25};  // declaring inputs and outputs
 const int inputCount = sizeof(inputs) / sizeof(inputs[0]);
@@ -58,20 +76,15 @@ byte outputs[] = {4, 14, 15, 27, 26};  // row
 const int outputCount = sizeof(outputs) / sizeof(outputs[0]);
 
 // Function declaration
-void initKeys();
-void lightSleepSetup();
-void renderScreen(char *msg);
+void initKeys(uint8_t keyLayout[ROWS][COLS], String keyInfo[ROWS][COLS]);
+void goSleeping();
+void renderScreen(String msg);
 void keyPress(Key &key);
 void keyRelease(Key &key);
 void showBatteryState();
-
-float getBatteryPercentage() {
-    float minVoltage = 3.700;
-    float fullVolatge = 4.200;
-    float batteryVolatage = tp.GetBatteryVoltage();
-
-    return (batteryVolatage - minVoltage) / (fullVolatge - minVoltage) * 100;
-}
+void breathLEDAnimation();
+float getBatteryVoltage();
+int getBatteryPercentage();
 
 void setup() {
     Serial.begin(115200);
@@ -86,34 +99,33 @@ void setup() {
     u8g2.begin();
 
     Serial.println("Configuring input pin...");
-    initKeys();
+    initKeys(keyLayout_2, keyInfo_2);
 
-    Serial.println("Configuring wake method...");
-    lightSleepSetup();
-
-    // Sleep test
-    // Serial.println("Go to sleep after 1 sec!");
-    // delay(1000);
-    // esp_light_sleep_start();
+    esp_sleep_enable_ext1_wakeup(0x8000, ESP_EXT1_WAKEUP_ANY_HIGH);
 }
 
 void loop() {
-    renderScreen((char *)"Connecting..");
+    renderScreen("Connecting..");
+    breathLEDAnimation();
 
-    if (bleKeyboard.isConnected()) renderScreen((char *)"= Connected =");
+    if (bleKeyboard.isConnected()) {
+        renderScreen("= Connected =");
+        tp.DotStar_SetPower(false);
+    }
 
-    // Check every keystroke is pressed or not
+    // Check every keystroke is pressed or not when connected
     while (bleKeyboard.isConnected()) {
         for (int r = 0; r < ROWS; r++) {
             digitalWrite(outputs[r], LOW);  // Setting one row low
-            delayMicroseconds(5);  // Time for electronics to settle down
-
             for (int c = 0; c < COLS; c++) {
                 if (digitalRead(inputs[c]) == ACTIVE) {
-                    // Calling keyPressed function if one of the inputs
-                    // reads low
-                    if (r == 4 && c == 0) {
-                        // Show battery on LED if the specific key is pressed
+                    // Calling keyPressed function if one of the inputs reads
+                    // low
+                    if (r == 1 && c == 0) {
+                        goSleeping();
+                    }
+                    else if (r == 4 && c == 0) {
+                        // Show battery level if the Fn key is pressed
                         showBatteryState();
                     } else {
                         keyPress(keyMap[r][c]);
@@ -121,25 +133,19 @@ void loop() {
                 } else {
                     keyRelease(keyMap[r][c]);
                 }
+                delayMicroseconds(5);
             }
-
             digitalWrite(outputs[r], HIGH);  // Setting the row back to high
             delayMicroseconds(5);
         }
-        delay(10);
     }
-
-    delay(500);
 }
 
 /**
  * Initialize every Key instance that used in this program
  *
  */
-void initKeys() {
-    // KeyC.keyStroke = 'b';
-    // KeyC.state = false;
-
+void initKeys(uint8_t keyLayout[ROWS][COLS], String keyInfo[ROWS][COLS]) {
     for (int i = 0; i < outputCount; i++) {
         pinMode(outputs[i], OUTPUT);
         digitalWrite(outputs[i], HIGH);
@@ -159,14 +165,17 @@ void initKeys() {
 }
 
 /**
- * Enable ESP wakeup for light sleep mode
+ * Enter deep sleep mode
  *
  */
-void lightSleepSetup() {
-    gpio_wakeup_enable(GPIO_NUM_32, GPIO_INTR_LOW_LEVEL);
-    gpio_wakeup_enable(GPIO_NUM_33, GPIO_INTR_LOW_LEVEL);
-    gpio_wakeup_enable(GPIO_NUM_14, GPIO_INTR_LOW_LEVEL);
-    esp_sleep_enable_gpio_wakeup();
+void goSleeping() {
+    u8g2.clearBuffer();
+    u8g2.sendBuffer();
+    esp_sleep_pd_config(ESP_PD_DOMAIN_RTC_PERIPH, ESP_PD_OPTION_ON);
+    gpio_pulldown_en(GPIO_NUM_15);
+    // gpio_pulldown_en(GPIO_NUM_27);
+    // gpio_pulldown_en(GPIO_NUM_26);
+    esp_deep_sleep_start();
 }
 
 /**
@@ -179,7 +188,7 @@ void keyPress(Key &key) {
         bleKeyboard.press(key.keyStroke);
     }
     key.state = true;
-    // renderScreen(key.keyInfo);
+    renderScreen(key.keyInfo);
 }
 
 /**
@@ -190,7 +199,6 @@ void keyPress(Key &key) {
 void keyRelease(Key &key) {
     if (key.state == true) {
         bleKeyboard.release(key.keyStroke);
-        // renderScreen((char *)"Waiting for input...");
     }
     key.state = false;
     return;
@@ -201,23 +209,20 @@ void keyRelease(Key &key) {
  *
  */
 void showBatteryState() {
-    tp.DotStar_SetPower(true);
-    float batteryV = getBatteryPercentage();
-    Serial.println((String)batteryV + "%");
-    tp.DotStar_SetBrightness(50);
-    if (batteryV > 90) {
-        tp.DotStar_SetPixelColor(0, 255, 0);
-    } else if (batteryV > 75) {
-        tp.DotStar_SetPixelColor(0, 200, 0);
-    } else if (batteryV > 50) {
-        tp.DotStar_SetPixelColor(219, 245, 0);
-    } else if (batteryV > 25) {
-        tp.DotStar_SetPixelColor(236, 141, 0);
+    String result = "";
+    bool plugged = digitalRead(9);
+    bool charging = tp.IsChargingBattery();
+    if (plugged && charging) {
+        result = "Charged";
+    } else if (plugged) {
+        result = "Plugged in";
     } else {
-        tp.DotStar_SetPixelColor(255, 0, 0);
+        int batteryPercentage = getBatteryPercentage();
+        result = "Bat. " + String(batteryPercentage) + "%";
     }
-    delay(1000);
-    tp.DotStar_SetPower(false);
+    renderScreen(result);
+    Serial.println(result);
+    delay(100);
 }
 
 /**
@@ -225,12 +230,17 @@ void showBatteryState() {
  *
  * @param {char} array to print on oled screen
  */
-void renderScreen(char *msg) {
+void renderScreen(String msg) {
+    int n = msg.length();
+    char char_array[n + 1];
+    // string to char array
+    strcpy(char_array, msg.c_str());
+
     u8g2.clearBuffer();
     u8g2.setFont(u8g2_font_ncenB08_tr);
     u8g2.setFontPosCenter();
-    u8g2.drawStr(64 - u8g2.getStrWidth(msg) / 2, 32, msg);
-    u8g2.setCursor(0, 15);
+    u8g2.drawStr(64 - u8g2.getStrWidth(char_array) / 2, 16, char_array);
+    // u8g2.setCursor(0, 15);
     // float bat = tp.GetBatteryVoltage();
     // if (tp.IsChargingBattery())
     //     u8g2.print((char *)"Charging");
@@ -238,4 +248,61 @@ void renderScreen(char *msg) {
     //     u8g2.print((char *)"Not Charging");
     // u8g2.print(bat, 2);
     u8g2.sendBuffer();
+}
+
+/**
+ * Return Battery Current Voltage
+ *
+ */
+float getBatteryVoltage() { return tp.GetBatteryVoltage(); }
+
+/**
+ * Return Battery Current Percentage
+ *
+ */
+int getBatteryPercentage() {
+    const float minVoltage = 3.15, fullVolatge = 4.05;
+    // Get average battery voltage value from 10 time periods for more stable
+    // result
+    float batteryVoltage = 0;
+    for (int i = 0; i < 100; i++) {
+        batteryVoltage += getBatteryVoltage();
+        delayMicroseconds(1);
+    }
+    batteryVoltage /= 100.0;
+
+    Serial.println(batteryVoltage);
+
+    // Convert to percentage
+    float percentage =
+        (batteryVoltage - minVoltage) / (fullVolatge - minVoltage) * 100;
+
+    // percentage =
+    //     percentage >= 100
+    //         ? 100
+    //         : static_cast<int>((percentage + 0.5 - (percentage < 0)) + 5);
+
+    // Update device's battery level
+    bleKeyboard.setBatteryLevel(percentage);
+
+    return percentage > 100 ? 100 : percentage;
+}
+
+/**
+ * Breath LED Animation
+ *
+ */
+void breathLEDAnimation() {
+    int brightness = 0;
+    tp.DotStar_SetPower(true);
+    // Brighten LED step by step
+    for (; brightness <= 50; brightness++) {
+        tp.DotStar_SetPixelColor(0, brightness, 0);
+        delayMicroseconds(5);
+    }
+    // Dimming LED step by step
+    for (; brightness >= 0; brightness--) {
+        delayMicroseconds(5);
+        tp.DotStar_SetPixelColor(0, brightness, 0);
+    }
 }
